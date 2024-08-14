d6ae595ad921e09045dd613bf4e59245633e8579
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-08-15 00:35:20 +0600

    created product schema, model, and post route

---
+++ models/product.ts
@@ -0,0 +1,53 @@
+import { Document, Schema, model } from "mongoose";
+
+// interface for the product
+export interface Product extends Document {
+	title: string;
+	image: string;
+	description: string;
+	price: number;
+	category: string;
+	ratings: number;
+	createdAt: Date;
+}
+
+// define the schema for the product
+const productSchema = new Schema<Product>({
+	title: {
+		type: String,
+		required: [true, "Provide the product title!"],
+	},
+	image: {
+		type: String,
+		required: [true, "Provide the product image URL!"],
+	},
+	description: {
+		type: String,
+		required: [true, "Provide the product description!"],
+	},
+	price: {
+		type: Number,
+		required: [true, "Provide the product price!"],
+	},
+	category: {
+		type: String,
+		required: [true, "Provide the product category!"],
+	},
+	ratings: {
+		type: Number,
+		default: 0,
+		min: [0, "Ratings cannot be less than 0"],
+		max: [5, "Ratings cannot be more than 5"],
+		validate: {
+			validator: (value: any) =>
+				typeof value === "number" && !isNaN(value),
+			message: "Ratings must be a valid number!",
+		},
+	},
+	createdAt: { type: Date, default: Date.now },
+});
+
+// create product model
+const ProductModel = model<Product>("Product", productSchema);
+
+export default ProductModel;
--- routes/products.ts
+++ routes/products.ts
@@ -1,7 +1,50 @@
 import express, { Request, Response } from "express";
+import ProductModel from "../models/product";
 
 const router = express.Router();
 
+interface ProductReqBody {
+	title: string;
+	image: string;
+	description: string;
+	price: number;
+	category: string;
+	ratings: number;
+}
+
+router.post(
+	"/",
+	async (req: Request<{}, {}, ProductReqBody>, res: Response) => {
+		try {
+			const newProduct = new ProductModel(req.body);
+			const product = await newProduct.save();
+			if (product?._id) {
+				return res
+					.status(201)
+					.send({
+						success: true,
+						message: `${product.title} is Saved Successfully!`,
+					});
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				console.error(error.message);
+				res.status(400).send({
+					success: false,
+					message: error.message,
+				});
+			} else {
+				console.error("An Unknown Error Occurred!");
+				res.status(500).send({
+					success: false,
+					message: "Internal Server Error!",
+				});
+			}
+		}
+	}
+);
+
+// dummy get route for products
 router.get("/", async (req: Request, res: Response) => {
 	try {
 		return res.status(200).send({
